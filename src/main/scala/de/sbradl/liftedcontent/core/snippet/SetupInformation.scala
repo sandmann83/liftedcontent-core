3aca4265008e8534cd8159891f3d70b72467be2a
Stefan Bradl <stefan_bradl@web.de>
2012-02-10 21:12:03 +0100

    title and subtitle are now content-editable for admins

--- src/main/scala/de/sbradl/liftedcontent/core/snippet/SetupInformation.scala
+++ src/main/scala/de/sbradl/liftedcontent/core/snippet/SetupInformation.scala
@@ -4,30 +4,52 @@ import scala.xml.NodeSeq
 import net.liftweb.util.Helpers._
 import net.liftweb.common.Empty
 import net.liftweb.common.Full
+import de.sbradl.liftedcontent.core.model.User
+import net.liftweb.http.SHtml
+import net.liftweb.http.js.JE.ElemById
+import net.liftweb.http.js.JsCmd
+import de.sbradl.liftedcontent.core.model.{ SetupInformation => SetupInformationModel }
+import net.liftweb.util.ClearNodes
+import net.liftweb.http.S
 
 class SetupInformation {
-  
+
+  val information = SetupInformationModel.find().openOr(
+    SetupInformationModel.create
+      .title("LiftedContent")
+      .subtitle("Scala/Lift based CMS"))
+
+  val enableEditing = User.superUser_?
+
   def render = {
-    "data-lift-id=title" #> name &
-    "data-lift-id=subtitle" #> subtitle
-  }
-  
-  private def name: String = information match {
-    case Empty => "LiftedContent"
-    case Full(info) => info.name
-  }
-  
-  private def subtitle: String = information match {
-    case Empty => "Scala/Lift based CMS"
-    case Full(info) => info.subtitle
-  }
-  
-  private def information = {
-    val data = de.sbradl.liftedcontent.core.model.SetupInformation.findAll()
+    val titleId = nextFuncName
+    val subtitleId = nextFuncName
     
-    data match {
-      case List() => Empty
-      case List(info) => Full(info)
+    "data-lift-id=title [contenteditable]" #> enableEditing &
+      "data-lift-id=title [id]" #> titleId &
+      "data-lift-id=title [onblur]" #> SHtml.ajaxCall(ElemById(titleId, "innerHTML"), saveTitle _) &
+      "data-lift-id=title *" #> information.title &
+      "data-lift-id=subtitle [contenteditable]" #> enableEditing &
+      "data-lift-id=subtitle [id]" #> subtitleId &
+      "data-lift-id=subtitle [onblur]" #> SHtml.ajaxCall(ElemById(subtitleId, "innerHTML"), saveSubTitle _) &
+      "data-lift-id=subtitle *" #> information.subtitle
+  }
+
+  private def saveTitle(text: String): JsCmd = {
+    if(enableEditing) {
+      information.title(text)
+      
+      information.save
+      S.notice(S ? "SAVED_CHANGES")
+    }
+  }
+  
+  private def saveSubTitle(text: String): JsCmd = {
+    if(enableEditing) {
+      information.subtitle(text)
+      
+      information.save
+      S.notice(S ? "SAVED_CHANGES")
     }
   }
 }
