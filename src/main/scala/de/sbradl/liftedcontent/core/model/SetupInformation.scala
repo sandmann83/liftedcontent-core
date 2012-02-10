52723f155ebdef3f5cb5034913837a935b828002
Stefan Bradl <stefan_bradl@web.de>
2012-02-10 21:11:11 +0100

    removed description from model

--- src/main/scala/de/sbradl/liftedcontent/core/model/SetupInformation.scala
+++ src/main/scala/de/sbradl/liftedcontent/core/model/SetupInformation.scala
@@ -3,14 +3,14 @@ import net.liftweb.mapper._
 import net.liftweb.http.S
 
 object SetupInformation extends SetupInformation with LongKeyedMetaMapper[SetupInformation] {
-  override def fieldOrder = List(name, subtitle, description)
+  override def fieldOrder = List(title, subtitle)
 }
 
 class SetupInformation extends LongKeyedMapper[SetupInformation] with IdPK {
 
   def getSingleton = SetupInformation
 
-  object name extends MappedString(this, 64) {
+  object title extends MappedString(this, 64) {
     override def displayName = S ? "WEBSITE_TITLE"
     override def validations = List(
       valMinLen(3, S ?? "NAME_TOO_SHORT") _,
@@ -21,9 +21,5 @@ class SetupInformation extends LongKeyedMapper[SetupInformation] with IdPK {
     override def displayName = S ? "WEBSITE_SUBTITLE"
     override def validations = List(
       valMaxLen(128, S ?? "NAME_TOO_LONG") _)
-  }
-
-  object description extends MappedTextarea(this, 512) {
-    override def displayName = S ? "WEBSITE_DESCRIPTION"
   }
 }
