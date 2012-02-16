c9eb94a095d55d15cd1466287d8b7995bc737506
Stefan Bradl <stefan_bradl@web.de>
2012-02-16 16:07:10 +0100

    -

--- src/main/scala/de/sbradl/liftedcontent/pages/snippet/Page.scala
+++ src/main/scala/de/sbradl/liftedcontent/pages/snippet/Page.scala
@@ -6,6 +6,7 @@ import de.sbradl.liftedcontent.pages.model.PageContent
 import net.liftweb.http.js.JE.JsRaw
 import net.liftweb.http.js.JsCmd.unitToJsCmd
 import net.liftweb.http.js.JsCmd
+import net.liftweb.http.js.JsCmds._
 import net.liftweb.http.S
 import net.liftweb.http.SHtml
 import net.liftweb.mapper.MappedField.mapToType
@@ -17,6 +18,8 @@ import de.sbradl.liftedcontent.rte.snippet.RichTextEditor
 import net.liftweb.util.PassThru
 import scala.xml.NodeSeq
 import net.liftweb.util.ClearNodes
+import net.liftweb.http.js.JE.JsEq
+import net.liftweb.http.js.JE.JsArray
 
 class Page(p: PageContent) {
 
@@ -25,14 +28,20 @@ class Page(p: PageContent) {
     case "" => S ? ("CLICK_TO_EDIT", p.language.isAsLocale)
     case _ => p.text
   }
-  
-  def saveContent(text: String): JsCmd = {
-    p.text(text)
+
+  def saveContent(t: String, value: String, inner: String): JsCmd = {
+
+    if("textarea" == t.toLowerCase) {
+      p.text(value)
+    } else {
+      p.text(inner)
+    }
+ 
     p.save
-    
+
     S.notice(S ? "SAVED_CHANGES")
   }
-  
+
   def saveTitle(title: String): JsCmd = {
     p.title(title)
     p.save
@@ -43,18 +52,23 @@ class Page(p: PageContent) {
   def render = {
     val titleId = nextFuncName
     val contentId = nextFuncName
-    
+
     val enableEditing = User.superUser_?
 
     "data-lift-id=title *" #> p.title &
-    "data-lift-id=title [id]" #> titleId &
-    "data-lift-id=title [contenteditable]" #> enableEditing &
-    "data-lift-id=title [onblur]" #> SHtml.ajaxCall(ElemById(titleId, "innerHTML"), saveTitle _) &
-    "data-lift-id=editor" #> (if(enableEditing) PassThru else ClearNodes) &
+      "data-lift-id=title [id]" #> titleId &
+      "data-lift-id=title [contenteditable]" #> enableEditing &
+      "data-lift-id=title [onblur]" #> SHtml.ajaxCall(ElemById(titleId, "innerHTML"), saveTitle _) &
+      "data-lift-id=editor" #> (if (enableEditing) PassThru else ClearNodes) &
       "data-lift-id=content [id]" #> contentId &
       "data-lift-id=content [contenteditable]" #> enableEditing &
-      "data-lift-id=content [class+]" #> (if (User.superUser_?) {"editable"} else {""}) &
+      "data-lift-id=content [designMode]" #> (if (enableEditing) "on" else "off") &
+      "data-lift-id=content [class+]" #> (if (User.superUser_?) { "editable" } else { "" }) &
       "data-lift-id=content *" #> Unparsed(displayContent) &
-      "data-lift-id=content [onblur]" #> SHtml.ajaxCall(ElemById(contentId, "innerHTML"), saveContent _)
+      "data-lift-id=content [onblur]" #> SHtml.ajaxCall(
+        JsArray(ElemById(contentId, "type"), ElemById(contentId, "value"), ElemById(contentId, "innerHTML")), (s) => {
+          val parts = s.split(",")
+          saveContent(parts(0), parts(1), parts(2))
+        })
   }
 }
