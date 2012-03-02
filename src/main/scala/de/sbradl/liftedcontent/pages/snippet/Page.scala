11bc9566503b2523d1a14e2b70d4a9b08fdd398e
Stefan Bradl <stefan_bradl@web.de>
2012-03-02 22:36:24 +0100

    moved to textile rte

--- src/main/scala/de/sbradl/liftedcontent/pages/snippet/Page.scala
+++ src/main/scala/de/sbradl/liftedcontent/pages/snippet/Page.scala
@@ -20,6 +20,8 @@ import scala.xml.NodeSeq
 import net.liftweb.util.ClearNodes
 import net.liftweb.http.js.JE.JsEq
 import net.liftweb.http.js.JE.JsArray
+import net.liftweb.textile.TextileParser
+import scala.xml.Text
 
 class Page(p: PageContent) {
 
@@ -29,14 +31,10 @@ class Page(p: PageContent) {
     case _ => p.text
   }
 
-  def saveContent(t: String, value: String, inner: String): JsCmd = {
+  def saveContent(value: String): JsCmd = {
+    p.text(value)
+    println("Content: " + value)
 
-    if("textarea" == t.toLowerCase) {
-      p.text(value)
-    } else {
-      p.text(inner)
-    }
- 
     p.save
 
     S.notice(S ? "SAVED_CHANGES")
@@ -61,14 +59,8 @@ class Page(p: PageContent) {
       "data-lift-id=title [onblur]" #> SHtml.ajaxCall(ElemById(titleId, "innerHTML"), saveTitle _) &
       "data-lift-id=editor" #> (if (enableEditing) PassThru else ClearNodes) &
       "data-lift-id=content [id]" #> contentId &
-      "data-lift-id=content [contenteditable]" #> enableEditing &
-      "data-lift-id=content [designMode]" #> (if (enableEditing) "on" else "off") &
       "data-lift-id=content [class+]" #> (if (User.superUser_?) { "editable" } else { "" }) &
-      "data-lift-id=content *" #> Unparsed(displayContent) &
-      "data-lift-id=content [onblur]" #> SHtml.ajaxCall(
-        JsArray(ElemById(contentId, "type"), ElemById(contentId, "value"), ElemById(contentId, "innerHTML")), (s) => {
-          val parts = s.split(",")
-          saveContent(parts(0), parts(1), parts(2))
-        })
+      "data-lift-id=content *" #> (if (enableEditing) Text(displayContent) else TextileParser.toHtml(displayContent)) &
+      "data-lift-id=content [onblur]" #> SHtml.ajaxCall(ElemById(contentId, "value"), saveContent _)
   }
 }
