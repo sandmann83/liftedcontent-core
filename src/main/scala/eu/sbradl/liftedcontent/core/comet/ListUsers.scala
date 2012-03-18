92ccea4dcc945d93df6291ed0027cc58d30f0bce
Stefan Bradl <stefan_bradl@web.de>
2012-03-18 13:21:28 +0100

    -

--- src/main/scala/eu/sbradl/liftedcontent/core/comet/ListUsers.scala
+++ src/main/scala/eu/sbradl/liftedcontent/core/comet/ListUsers.scala
@@ -6,6 +6,7 @@ import eu.sbradl.liftedcontent.core.lib.UserManagementHelpers.deleteUser
 import eu.sbradl.liftedcontent.core.lib.UserAddedOrDeleted
 import eu.sbradl.liftedcontent.core.lib.UserServer
 import eu.sbradl.liftedcontent.core.model.User
+import eu.sbradl.liftedcontent.util.DisplayIf
 import eu.sbradl.liftedcontent.util.OnConfirm
 
 import net.liftweb.http.CometActor
@@ -15,8 +16,6 @@ import net.liftweb.http.SHtml
 import net.liftweb.mapper.MappedField.mapToType
 import net.liftweb.util.IterableConst.itNodeSeqFunc
 import net.liftweb.util.StringPromotable.jsCmdToStrPromo
-import net.liftweb.util.ClearNodes
-import net.liftweb.util.PassThru
 
 class ListUsers extends CometActor with CometListener {
   
@@ -34,7 +33,7 @@ class ListUsers extends CometActor with CometListener {
             "data-lift-id=status *" #> SHtml.ajaxCheckbox(user.validated, (value) => activateOrDeactivateUser(user.id.is, value)) &
             "data-lift-id=roles [href]" #> ("/admin/users/manage-roles/" + user.id.is) &
             "data-lift-id=roles [title]" #> (S ? "MANAGE_ROLES") &
-            "data-lift-id=delete" #> (if(canDelete(user)) PassThru else ClearNodes) &
+            "data-lift-id=delete" #> DisplayIf(canDelete(user)) &
             "data-lift-id=delete [onclick]" #> OnConfirm(S ? "REALLY_DELETE_USER", () => deleteUser(user)) &
             "data-lift-id=delete [title]" #> (S ? "DELETE")
         }
--- src/main/scala/eu/sbradl/liftedcontent/pages/snippet/Page.scala
+++ src/main/scala/eu/sbradl/liftedcontent/pages/snippet/Page.scala
@@ -1,27 +1,22 @@
 package eu.sbradl.liftedcontent.pages.snippet
 
-import scala.xml.Unparsed
 import eu.sbradl.liftedcontent.core.model.User
 import eu.sbradl.liftedcontent.pages.model.PageContent
-import net.liftweb.http.js.JE.JsRaw
+import eu.sbradl.liftedcontent.util.DisplayIf
+
+import scala.xml.Text
+
+import net.liftweb.http.js.JE.ElemById
 import net.liftweb.http.js.JsCmd.unitToJsCmd
 import net.liftweb.http.js.JsCmd
-import net.liftweb.http.js.JsCmds._
 import net.liftweb.http.S
 import net.liftweb.http.SHtml
 import net.liftweb.mapper.MappedField.mapToType
-import net.liftweb.util.Helpers._
-import net.liftweb.util.StringPromotable.booleanToStrPromo
-import net.liftweb.http.js.JE.ElemById
-import net.liftweb.http.js.JsCmds
-import eu.sbradl.liftedcontent.rte.snippet.RichTextEditor
-import net.liftweb.util.PassThru
-import scala.xml.NodeSeq
-import net.liftweb.util.ClearNodes
-import net.liftweb.http.js.JE.JsEq
-import net.liftweb.http.js.JE.JsArray
 import net.liftweb.textile.TextileParser
-import scala.xml.Text
+import net.liftweb.util.Helpers.nextFuncName
+import net.liftweb.util.Helpers.strToCssBindPromoter
+import net.liftweb.util.StringPromotable.booleanToStrPromo
+import net.liftweb.util.StringPromotable.jsCmdToStrPromo
 
 class Page(p: PageContent) {
 
@@ -55,7 +50,7 @@ class Page(p: PageContent) {
       "data-lift-id=title [id]" #> titleId &
       "data-lift-id=title [contenteditable]" #> enableEditing &
       "data-lift-id=title [onblur]" #> SHtml.ajaxCall(ElemById(titleId, "innerHTML"), saveTitle _) &
-      "data-lift-id=editor" #> (if (enableEditing) PassThru else ClearNodes) &
+      "data-lift-id=editor" #> DisplayIf((enableEditing)) &
       "data-lift-id=content [id]" #> contentId &
       "data-lift-id=content [class+]" #> (if (User.superUser_?) { "editable" } else { "" }) &
       "data-lift-id=content *" #> (if (enableEditing) Text(displayContent) else TextileParser.toHtml(displayContent)) &
